476218f78c4a24fcec928a2b7c62aafc690c99ff
Alex <usovich_al@mail.ru>
2023-06-27 15:43:24 +0300

    slider-blog

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-const mixer = mixitup('.portfolio__content');
-Fancybox.bind()
+$(function(){
+
+    const mixer = mixitup('.portfolio__content');
+
+    Fancybox.bind();
+
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.svg" alt="arrow-right"></button>'
+      });
+
+})
+
